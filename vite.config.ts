7426369bc116e705a593aa3bfad90c166b8697d5
Arnau Sanchez <pyarnau@gmail.com>
2023-04-02 19:12:28 +0200

    Add .env load with port

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import eslint from "vite-plugin-eslint";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react(), eslint()],
-});
+
+export default ({ mode }) => {
+    const env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+    return defineConfig({
+        plugins: [react(), eslint()],
+
+        server: {
+            port: parseInt(env.VITE_PORT),
+        },
+    });
+};
