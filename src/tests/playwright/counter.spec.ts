148b4851e2cd4f1972b145f0819d757433b7e266
Arnau Sanchez <pyarnau@gmail.com>
2023-04-27 20:03:17 +0200

    CI

--- src/tests/playwright/counter.spec.ts
+++ src/tests/playwright/counter.spec.ts
@@ -1,7 +1,7 @@
 import { test } from "@playwright/test";
 
 test("test", async ({ page }) => {
-    await page.goto("http://localhost:5000/");
+    await page.goto("/");
 
     await page.getByRole("link", { name: "Counter 1" }).click();
     await page.getByRole("button", { name: "+1" }).click();
