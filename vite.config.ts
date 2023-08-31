eafd7a572b2da0ffd902f15ed9cfca8a43eba4e0
Arnau Sanchez <pyarnau@gmail.com>
2023-08-31 19:25:51 +0200

    Add vite-plugin-checker

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig, loadEnv } from "vite";
 import eslint from "vite-plugin-eslint";
 import react from "@vitejs/plugin-react";
+import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 
@@ -9,7 +10,17 @@ export default ({ mode }) => {
     const env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
     return defineConfig({
-        plugins: [react(), eslint()],
+        plugins: [
+            react(),
+            eslint(),
+            checker({
+                typescript: true,
+                eslint: {
+                    lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+                    dev: { logLevel: ["error"] },
+                },
+            }),
+        ],
         test: {
             environment: "jsdom",
             setupFiles: "./src/tests/setup.js",
